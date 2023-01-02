a8eb3b1fde933e37ad72feece2c568c0f014718a
Luis <lvas248@gmail.com>
2023-01-02 16:48:38 -0500

    commit tree 2d7cc2b8d25b5b40a233927c9f0438affbfb738f

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,6 +1,17 @@
+require 'pry'
+
 def fibonacci(num)
   # type your code in here
+  arr =[0,1]
+  i = 0
+  while arr.length <= num 
+    n = arr[i-2] + arr[i-1]
+    arr<<n
+  end
+
+  arr.last
 end
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 0"
@@ -20,4 +31,17 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+
+#Take the argument num
+#create an array with the first 2 values of the fib sequence
+#run the fibonacci sequence num times
+#each time it cycles, append value to array
+#return the value at the numth index of the array
+
+
 # And a written explanation of your solution
+
+#To solve this i decided to create an array with the first two values of the Fib sequence.
+#Then i wrote a formula that cycles through each integer and finds the value at that index of the squence.
+# It then appends it to the original array.
+# once theh loop ends, it returns the value of the array at the last index.
